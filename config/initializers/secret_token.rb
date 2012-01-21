725c324826fac09a990ac2cd41f170fd5b6fd4b3
Jessica Griffin <furiana@yahoo.com>
2012-01-20 16:10:51 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Furiana::Application.config.secret_token = '80dbc2478dda0de6f17b1b0e5f24efe660b91136e89805bec173cf174bbd3daac85d0ac0f189b779f4158e264b560732536c65447a9bdd2da361f16612e79f6f'
